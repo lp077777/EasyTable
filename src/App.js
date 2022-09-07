83c75d351977538227caddefe0e7599501158f70
junyi <jzhang5@ltu.edu>
2022-09-06 21:40:00 -0500

    easy table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from "react"
+import { customerList } from './customer'
+
+const redSolid = {border:'solid' ,borderColor:'red'}
+
+function Table({ props}) { 
+  return (
+    <div>
+      <div>Table Number: {props.table} </div>
+      Guests: <div style={{display:'flex' ,flexDirection:"column"}}>
+      {props.customer.map(customer => { 
+        return (
+          <div style={customer.checked? redSolid:null} key={customer.key}>
+            {customer.name}
+          </div>
+        )
+      })}
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
 
 function App() {
+  const [customers, setCustomers] = useState([])
+  const [searchValue,setSearchValue ] = useState('')
+  useEffect(() => { 
+    setCustomers(customerList)
+  }, [])
+  const onChangeUpdate = (e) => { 
+    setSearchValue(e.target.value)
+  }
+  const checkName = () => { 
+    setSearchValue('')
+    let updatedCustomerList = [...customers]
+    for (let i = 0; i < updatedCustomerList.length; i++) { 
+      let tableGuest = updatedCustomerList[i].customer
+      for (let j = 0; j < tableGuest.length; j++) { 
+        if (tableGuest[j].name === searchValue) {
+          tableGuest[j].checked = true
+          return setCustomers(updatedCustomerList)} 
+      }
+    }
+    alert('You have entered a wrong name')
+  }
+  const removeName = () => { 
+    setSearchValue('')
+    let updatedCustomerList = [...customers]
+    for (let i = 0; i < updatedCustomerList.length; i++) { 
+      let tableGuest = updatedCustomerList[i].customer
+      for (let j = 0; j < tableGuest.length; j++) { 
+        if (tableGuest[j].name === searchValue) {
+          tableGuest[j].checked = false
+          return setCustomers(updatedCustomerList)} 
+      }
+    }
+    alert('You have entered a wrong name')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>
+      Table
+      </h3>
+      <div style={{ margin:'30px'}}>
+        <input
+          value={searchValue}
+          onChange={onChangeUpdate} />
+        <button onClick={checkName}>Check</button>
+        <button onClick={removeName}>UnChecked</button>
+      </div>
+      <div style={{display:'flex', justifyContent:'space-evenly'}}>
+        {
+          customers.map(table => { 
+            return (
+              <Table props={table} key={table.table}/>
+              )
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/customer.js
@@ -0,0 +1,35 @@
+export const customerList = [{
+    table: 1,
+    customer: [
+      { key: 't1c1',name: 'aaa', checked: false },
+      { key: 't1c2',name: 'aab', checked: false },
+      { key: 't1c3',name: 'aac', checked: false }]
+  },
+  {
+    table: 2,
+    customer: [
+      { key: 't2c1',name: 'aba', checked: false },
+      { key: 't2c2',name: 'abb', checked: false },
+      { key: 't2c3',name: 'abc', checked: false },
+      { key: 't2c4', name: 'abd', checked: false },
+      { key: 't2c5', name: 'abe', checked: false },
+      { key: 't2c6', name: 'abf', checked: false },
+      { key: 't2c7',name: 'abg', checked: false },]
+  },
+  {
+    table: 3,
+    customer: [
+      { key: 't3c1',name: 'aca', checked: false },
+      { key: 't3c2',name: 'acb', checked: false },
+      { key: 't3c3',name: 'acc', checked: false },
+      { key: 't3c4',name: 'acd', checked: false },]
+  },
+  {
+    table: 4,
+    customer: [
+      { key: 't4c1',name: 'ada', checked: false },
+      { key: 't4c2',name: 'adb', checked: false },
+      { key: 't4c3',name: 'adc', checked: false },
+      { key: 't4c4',name: 'add', checked: false },]
+  },
+]
